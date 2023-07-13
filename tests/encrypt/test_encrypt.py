fda852dba46c0700490bc2d81f14ec3b7d009cd7
Levy Araujo <levy4raujo@gmail.com>
2023-07-13 16:44:03 -0300

    [Req 02 Concluído]
    Criando Testes.

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    # Teste para Tipos incorretos
+    with pytest.raises(TypeError):
+        encrypt_message(3, 'Freva')
+    # Teste para Keys maiores que a string
+    assert encrypt_message('Freva', 8) == 'averF'
+    # Teste para valores Ímpares
+    assert encrypt_message('Freva', 1) == 'F_aver'
+    # Teste para valores Pares
+    assert encrypt_message('Freva', 2) == 'ave_rF'
