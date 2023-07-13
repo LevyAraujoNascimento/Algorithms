590a6c878f20d197cca685d1baa1f4174f9ef10b
Levy Araujo <levy4raujo@gmail.com>
2023-07-13 15:13:42 -0300

    [Req 01 Concluído]
    Study Schedule finalizado.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    if target_time is None or target_time == 0:
+        return None
+    for hour in permanence_period:
+        if not isinstance(hour[0], int) or not isinstance(hour[1], int):
+            count = None
+            break
+        else:
+            if hour[0] <= target_time <= hour[1]:
+                count += 1
+    return count
