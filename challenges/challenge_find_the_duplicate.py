b8a5fb15412c1a9fa4f5d017af2d1eb56222d7bb
Levy Araujo <levy4raujo@gmail.com>
2023-07-15 11:12:44 -0300

    [Req 05 Concluído]
    Busca Duplicidade.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,42 @@
+def quick_sort(array, start, end):
+    if start < end:
+        p = partition(array, start, end)
+        quick_sort(array, start, p - 1)
+        quick_sort(array, p + 1, end)
+
+
+def partition(array, start, end):
+    pivot = array[end]
+    delimiter = start - 1
+    for i in range(start, end):
+        if array[i] <= pivot:
+            delimiter = delimiter + 1
+            array[i], array[delimiter] = array[delimiter], array[i]
+    array[delimiter + 1], array[end] = array[end], array[delimiter + 1]
+    return delimiter + 1
+
+
+def test(num):
+    if num < 0:
+        return True
+    elif isinstance(num, str):
+        return True
+    else:
+        return False
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1 or isinstance(nums[0], str):
+        return False
+    count = 1
+    result = False
+    quick_sort(nums, 0, len(nums) - 1)
+    while count <= len(nums) - 1:
+        if test(nums[count]) or test(nums[count - 1]):
+            break
+        elif nums[count] == nums[count - 1]:
+            result = nums[count]
+            break
+        else:
+            count += 1
+    return result
