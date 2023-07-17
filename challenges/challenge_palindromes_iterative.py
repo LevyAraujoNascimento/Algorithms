60a6483e311e19725668f1959fe379a1845ff4ee
Levy Araujo <levy4raujo@gmail.com>
2023-07-17 10:02:06 -0300

    [Req 06 Concluído]
    Palindrome iterativo.

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,13 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+        return False
+    low_index = 0
+    high_index = len(word) - 1
+    result = True
+    while high_index != low_index and high_index > low_index:
+        if word[high_index] != word[low_index]:
+            result = False
+            break
+        low_index += 1
+        high_index -= 1
+    return result
